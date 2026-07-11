7e355ad3eeb4036b1896518d1bee93d6f79a7afe
DONGFANG WANGDAREN | 东方 旺大人 <16002223+DONGFANG-WANGDAREN@users.noreply.github.com>
2026-07-12 02:53:07 +0800

    T8::H3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.DONGFANG_WANGDAREN.Reader_RX"
         minSdk = 33
         targetSdk = 36
-        versionCode = 26_0711_22_2
-        versionName = "26_0711_22_2.NANJING"
+        versionCode = 26_0712_02_1
+        versionName = "26_0712_02_1.NANJING"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
